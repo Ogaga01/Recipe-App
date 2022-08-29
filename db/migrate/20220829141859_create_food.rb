8943d297a4b6f4bbf04a098de6148bbb7f13945c
Ogaga <iyaraogaga69@gmail.com>
2022-08-29 15:21:06 +0100

    Create Foods table

--- db/migrate/20220829141859_create_food.rb
+++ db/migrate/20220829141859_create_food.rb
@@ -2,6 +2,9 @@ class CreateFood < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
       t.string :name
+      t.string :measurement_unit
+      t.string :price
+      t.string :quantity
 
       t.timestamps
     end
