8415ca582789caa4a5dcd6f92ffee491d4a1e063
Ogaga <iyaraogaga69@gmail.com>
2022-08-29 17:16:05 +0100

    Add recipe Validation

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -2,7 +2,5 @@ class RecipeFood < ApplicationRecord
     belongs_to :food
     belongs_to :recipe
 
-    validates :name, presence: true
-    validates :preparation_time, presence: true
-    validates :cooking_time, presence: true
+    validates :quantity, presence: true
 end
