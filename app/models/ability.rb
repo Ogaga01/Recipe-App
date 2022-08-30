7c6a73c44a5f6afa8d5c403f7f2051b5e0b36b06
Ogaga <iyaraogaga69@gmail.com>
2022-08-30 12:19:52 +0100

    Cancan ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new
+
+    can :manage, Recipe do |recipe|
+      recipe.user_id == user.id
+    end
+
+    can :read, :all
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
