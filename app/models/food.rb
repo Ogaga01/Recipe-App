c5a261ff8e701a1b66d557780142f83ee2beb21a
Ogaga <iyaraogaga69@gmail.com>
2022-08-30 12:41:27 +0100

    update validation in food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,6 +4,9 @@ class Food < ApplicationRecord
 
   validates :name, presence: true
   validates :measurement_unit, presence: true
-  validates :price, presence: true
-  validates :quantity, presence: true
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  validates :quantity, presence: true,
+                       numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
