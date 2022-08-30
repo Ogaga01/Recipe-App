659bf0cecc39ff59768b2bce6eb9e85e098ad74c
Ogaga <iyaraogaga69@gmail.com>
2022-08-30 13:31:19 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'recipe/index'
+  get 'public_recipes/index'
+  # get 'recipe/index'
+  get 'shopping_lists/index'
   devise_for :users
 
   root 'home#index'
-  resources :recipes, except: [:update]
+  resources :recipes do
+    resources :recipe_foods
+  end
+
+  resources :users do
+    resources :recipes
+    resources :foods
+  end
+
+  resources :foods do
+    resources :reipe_foods
+  end
+
+  get '/public_recipes/index', to: 'public_recipes#index', as: :public_recipes
+  get '/shopping_lists/index', to: 'shopping_lists#index', as: :shopping_lists
 end
