7efa3edbf12fe02d23d29b0c5a2e6af44d211ccc
Ogaga <iyaraogaga69@gmail.com>
2022-08-30 12:39:48 +0100

    Change price data type to integer

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
-  has_many :recipe_food
+  has_many :recipe_food, dependent: :destroy
 
   validates :name, presence: true
   validates :measurement_unit, presence: true
