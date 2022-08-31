df87e9777b9e955b4170e261cea24b9d05faf8de
yasin <yasin.elsharawi23@gmail.com>
2022-08-31 13:30:04 +0300

    Resolve linter error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :foods
   has_many :recipes
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
